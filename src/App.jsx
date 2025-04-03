b9a317dfb7dd9fb69115d11749cea7a1a74f011e
13Kirat <141816179+13Kirat@users.noreply.github.com>
2025-04-03 20:54:20 +0530

    sponsors section completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,28 @@ import React from 'react'
 import { Navbar } from './components/Navbar'
 import { Hero } from './components/Hero'
 import { TimelineDemo } from './components/TimelineDemo'
+import { Logos3 } from './components/logos3'
 import TracksSlider from './components/tracks'
 
 import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-[#333]">
       <Navbar />
       <Hero />
       <div id="timeline">
         <TimelineDemo />
       </div>
-      
+
       {/* Add Tracks section here */}
       <div id="tracks" className="flex justify-center items-center w-full mt-16"> {/* Add margin-top for spacing */}
         <h2 className="text-center text-3xl font-bold text-white mb-10">Hackathon Tracks</h2>
         <TracksSlider />
+      </div>
+      <div className="">
+        <h2 className="text-center text-3xl font-bold text-white mb-10">Partners</h2>
+        <Logos3 />
       </div>
     </div>
   )
