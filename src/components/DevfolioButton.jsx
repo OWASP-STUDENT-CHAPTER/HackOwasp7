46c0f5919c12891996834c9ec69023b24be66167
VedantAnand17 <VEDANTANAND0077@gmail.com>
2025-04-08 23:02:47 +0530

    feat: Integrate DevfolioButton component into Countdown and Navbar

---
+++ src/components/DevfolioButton.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+const DevfolioButton = ({ hackathonSlug = "hackowasp7", theme = "light" }) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div 
+      className="apply-button" 
+      data-hackathon-slug={hackathonSlug} 
+      data-button-theme={theme}
+      style={{ height: '44px', width: '312px' }}
+    ></div>
+  );
+};
+
+export default DevfolioButton;
