ecc8d231bb9d11da919f0f387551917f5d45f01f
13Kirat <141816179+13Kirat@users.noreply.github.com>
2025-04-13 00:04:14 +0530

    content change

--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -43,14 +43,23 @@ export const Timeline = ({ data }) => {
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-black flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
-                {item.title}
-              </h3>
+              <div className="">
+                <h3 className="hidden md:block text-xl md:pl-20 md:text-4xl font-bold text-neutral-500 dark:text-neutral-500 ">
+                  {item.title}
+                  <br />
+                  <h3 className="hidden md:block text-lg md:text-2xl font-bold text-neutral-500 dark:text-neutral-500 ">
+                    {item?.subtitle}
+                  </h3>
+                </h3>
+              </div>
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
               <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
                 {item.title}
+                <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+                  {item?.subtitle}
+                </h3>
               </h3>
               {item.content}{" "}
             </div>
